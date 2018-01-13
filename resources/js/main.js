7664ec97b7a75c21c51666efacced4dab889c930
chanamur <cdoutreloux@gmail.com>
2018-01-13 15:13:11 +0100

    finish navbar animation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  //slick
   $('.carousel').slick({
     slidesToScroll: 1,
     autoplay: true,
@@ -7,5 +8,19 @@ $(document).ready(function(){
     draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false
+  });
+
+  //navbar
+  var scrollNavbar = 0;
+  $(window).scroll(function() {
+    var goingDown = $(this).scrollTop();
+    if (goingDown - scrollNavbar > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scrollNavbar = goingDown;
+    } else if (scrollNavbar - goingDown > 50) {
+      $('.navbar').animate({top: 0}, 150);
+      scrollNavbar = goingDown;
+    }
   });
 });
